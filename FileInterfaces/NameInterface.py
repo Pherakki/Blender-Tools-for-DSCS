2f2f5b6e29feccdd8713153cdc96bb45407aab51
Pherakki <pherakki@gmail.com>
2021-02-10 22:03:28 +0000

    Add NameInterface

---
+++ FileInterfaces/NameInterface.py
@@ -0,0 +1,41 @@
+from FileReaders.NameReader import NameReader
+
+
+class NameInterface:
+    def __init__(self):
+        self.bone_names = []
+        self.material_names = []
+
+    @classmethod
+    def from_file(cls, path):
+        with open(path, 'rb') as F:
+            namereader = NameReader(F)
+            namereader.read()
+
+        new_name_interface = cls()
+        new_name_interface.bone_names = namereader.bone_names
+        new_name_interface.material_names = namereader.material_names
+
+        return new_name_interface
+
+    def to_file(self, path):
+        with open(path, 'wb') as F:
+            namereader = NameReader(F)
+
+            bone_names = self.bone_names
+            material_names = self.material_names
+
+            namereader.num_bone_names = len(bone_names)
+            namereader.num_material_names = len(material_names)
+
+            num_ptrs = len(bone_names) + len(material_names)
+            namereader.bone_name_pointers = [8 + 4 * num_ptrs + sum([len(name) for name in bone_names[:i]])
+                                             for i in range(len(bone_names))]
+
+            namereader.material_name_pointers = [namereader.bone_name_pointers[-1] + len(bone_names[-1]) +
+                                                 sum([len(name) for name in material_names[:i]])
+                                                 for i in range(len(material_names))]
+            namereader.bone_names = bone_names
+            namereader.material_names = material_names
+
+            namereader.write()
