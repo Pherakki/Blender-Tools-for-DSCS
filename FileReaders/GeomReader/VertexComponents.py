7702a7f58a1e9ce674e9fc0ac89517d2765ca622
Pherakki <pherakki@gmail.com>
2021-02-14 05:03:42 +0000

    Add vertex component classes

---
+++ FileReaders/GeomReader/VertexComponents.py
@@ -0,0 +1,126 @@
+class BaseVertexComponent:
+    vertex_type = None
+    num_elements = None
+    vertex_dtype = None
+
+    def __init__(self, data_start_ptr):
+        self.data_start_ptr = data_start_ptr
+
+    @classmethod
+    def generator(cls, vtx):
+        assert len(vtx[cls.vertex_type]) == cls.num_elements, "Vertex has an invalid number of elements."
+        return {cls.vertex_type: vtx[cls.vertex_type]}
+
+
+class Position(BaseVertexComponent):
+    vertex_type = 'Position'
+    num_elements = 3
+    vertex_dtype = 'f'
+
+
+class PosWeight(BaseVertexComponent):
+    vertex_type = 'Position'
+    num_elements = 4
+    vertex_dtype = 'f'
+
+    def generator(self, vtx):
+        return {'Position': [*vtx['Position'], float(3 * vtx['WeightedBoneID'][0])]}
+
+
+class Normal(BaseVertexComponent):
+    vertex_type = 'Normal'
+    num_elements = 3
+    vertex_dtype = 'e'
+
+
+class Tangent(BaseVertexComponent):
+    vertex_type = 'Tangent'
+    num_elements = 4
+    vertex_dtype = 'e'
+
+
+class Binormal(BaseVertexComponent):
+    vertex_type = 'Binormal'
+    num_elements = 3
+    vertex_dtype = 'e'
+
+
+class UV(BaseVertexComponent):
+    vertex_type = 'UV'
+    num_elements = 2
+    vertex_dtype = 'e'
+
+    @classmethod
+    def generator(cls, vtx):
+        return {cls.vertex_type: (vtx[cls.vertex_type][0], 1 - vtx[cls.vertex_type][-1]) if vtx[cls.vertex_type] is not None else vtx[cls.vertex_type]}
+
+
+class UV2(BaseVertexComponent):
+    vertex_type = 'UV2'
+    num_elements = 2
+    vertex_dtype = 'e'
+
+    @classmethod
+    def generator(cls, vtx):
+        return {cls.vertex_type: (vtx[cls.vertex_type][0], 1 - vtx[cls.vertex_type][-1]) if vtx[cls.vertex_type] is not None else vtx[cls.vertex_type]}
+
+
+class UV3(BaseVertexComponent):
+    vertex_type = 'UV3'
+    num_elements = 2
+    vertex_dtype = 'e'
+
+    @classmethod
+    def generator(cls, vtx):
+        return {cls.vertex_type: (vtx[cls.vertex_type][0], 1 - vtx[cls.vertex_type][-1]) if vtx[cls.vertex_type] is not None else vtx[cls.vertex_type]}
+
+
+class Colour(BaseVertexComponent):
+    vertex_type = 'Colour'
+    num_elements = 4
+    vertex_dtype = 'e'
+
+
+class Indices2(BaseVertexComponent):
+    vertex_type = 'WeightedBoneID'
+    num_elements = 2
+    vertex_dtype = 'B'
+
+
+class Indices3(BaseVertexComponent):
+    vertex_type = 'WeightedBoneID'
+    num_elements = 3
+    vertex_dtype = 'B'
+
+
+class Indices4(BaseVertexComponent):
+    vertex_type = 'WeightedBoneID'
+    num_elements = 4
+    vertex_dtype = 'B'
+
+
+class Weights2(BaseVertexComponent):
+    vertex_type = 'BoneWeight'
+    num_elements = 2
+    vertex_dtype = 'e'
+
+
+class Weights3(BaseVertexComponent):
+    vertex_type = 'BoneWeight'
+    num_elements = 3
+    vertex_dtype = 'e'
+
+
+class Weights4(BaseVertexComponent):
+    vertex_type = 'BoneWeight'
+    num_elements = 4
+    vertex_dtype = 'e'
+
+
+all_vtx_components = [Position, PosWeight, Normal, UV, UV2, UV3, Colour, Tangent, Binormal,
+                      Indices2, Indices3, Indices4,
+                      Weights2, Weights3, Weights4]
+
+# These dictionaries are how the vertex components are accessed from other files via factory methods
+vertex_components_from_names = {cls.__name__: cls for cls in all_vtx_components}
+vertex_components_from_defn = {(cls.vertex_type, cls.num_elements, cls.vertex_dtype): cls for cls in all_vtx_components}
