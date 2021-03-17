49ff545a958cc3aca87f912d6b2ce1f42ed45198
Pherakki <pherakki@gmail.com>
2021-03-17 03:46:00 +0000

    Add lerp and slerp algorithms

---
+++ Utilities/Interpolation.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def lerp(x, y, t):
+    return (1-t)*x + t*y
+
+
+def slerp(x, y, t):
+    omega = np.arccos(np.dot(x, y))
+    if omega == 0:
+        return x
+    term_1 = x * np.sin((1-t)*omega)
+    term_2 = y * np.sin(t*omega)
+
+    return (term_1 + term_2) / np.sin(omega)
