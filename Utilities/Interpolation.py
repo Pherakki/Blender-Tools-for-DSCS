Fix a bug due to numerical instability

Utilities/Interpolation.py
@@ -7,7 +7,7 @@ def lerp(x, y, t):
 
 def slerp(x, y, t):
     omega = np.arccos(np.dot(x, y))
-    if omega == 0:
+    if omega == 0 or np.isnan(omega):
         return x
     term_1 = x * np.sin((1-t)*omega)
     term_2 = y * np.sin(t*omega)