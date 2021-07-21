d812895369e7e93ab16be3810d794efedb713674
Pherakki <pherakki@gmail.com>
2021-07-21 04:01:54 +0100

    Remove debug prints

--- BlenderIO/Import/CameraImport.py
+++ BlenderIO/Import/CameraImport.py
@@ -12,9 +12,6 @@ def import_cameras(parent_obj, model_data):
         camera.clip_start = camera_data.zNear
         camera.clip_end = camera_data.zFar
 
-        print(camera_data.bone_name_hash)
-        print(model_data.bone_name_hashes)
-
         camera_obj = bpy.data.objects.new(f"{parent_obj.name} Camera {i}", camera)
         bpy.context.collection.objects.link(camera_obj)
         camera_obj.parent = parent_obj
@@ -24,5 +21,5 @@ def import_cameras(parent_obj, model_data):
         target_bone_hash = hex(camera_data.bone_name_hash)[2:]
         target_bone_hash = (8-len(target_bone_hash))*'0' + target_bone_hash
         target_bone_hash = target_bone_hash[6:8] + target_bone_hash[4:6] + target_bone_hash[2:4] + target_bone_hash[0:2]
-        print(target_bone_hash)
+
         constraint.subtarget = model_data.bone_name_hashes[target_bone_hash]
