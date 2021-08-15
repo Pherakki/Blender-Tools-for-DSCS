59dc2cf6bf4838ea3ea6aa7e716210693fdb370d
Pherakki <71677509+Pherakki@users.noreply.github.com>
2021-08-15 23:53:28 +0100

    Add some OpenGL enums and functions

---
+++ Utilities/OpenGLResources.py
@@ -0,0 +1,62 @@
+id_to_glfunc = {
+    160: "glAlphaFunc",
+    161: "GL_ALPHA_TEST",
+    162: "glBlendFunc",
+    163: "glBlendEquationSeparate",
+    164: "GL_BLEND",
+    165: "glCullFace",
+    166: "GL_CULL_FACE",
+    167: "glDepthFunc",
+    168: "glDepthMask",
+    169: "GL_DEPTH_TEST",
+
+
+    172: "glColorMask"
+
+}
+
+glBool_options = {
+    1: "GL_TRUE",
+    0: "GL_FALSE"
+}
+
+glEnable_options = {
+    1: "glEnable",
+    0: "glDisable"
+}
+
+glBlendFunc_options = {
+    0x0000: "GL_ZERO",
+    0x0001: "GL_ONE",
+    0x0300: "GL_SRC_COLOR",
+    0x0301: "GL_ONE_MINUS_SRC_COLOR",
+    0x0302: "GL_SRC_ALPHA",
+    0x0303: "GL_ONE_MINUS_SRC_ALPHA",
+    0x0304: "GL_DST_ALPHA",
+    0x0305: "GL_ONE_MINUS_DST_ALPHA"
+}
+
+glBlendEquationSeparate_options = {
+    0x8006: "GL_FUNC_ADD",
+    0x800A: "GL_FUNC_SUBTRACT",
+    0x800B: "GL_FUNC_REVERSE_SUBTRACT",
+    0x8007: "GL_MIN",
+    0x8008: "GL_MAX"
+}
+
+glCullFace_options = {
+    0x0404: "GL_FRONT",
+    0x0405: "GL_BACK",
+    0x0408: "GL_FRONT_AND_BACK"
+}
+
+glComparison_options = {
+    0x0200: "GL_NEVER",
+    0x0201: "GL_LESS",
+    0x0202: "GL_EQUAL",
+    0x0203: "GL_LEQUAL",
+    0x0204: "GL_GREATER",
+    0x0205: "GL_NOTEQUAL",
+    0x0206: "GL_GEQUAL",
+    0x0207: "GL_ALWAYS"
+}
