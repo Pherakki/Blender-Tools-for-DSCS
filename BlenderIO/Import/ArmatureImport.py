26940a81988103e86edd120efe9d16cbcea1675a
Pherakki <71677509+Pherakki@users.noreply.github.com>
2021-09-20 22:37:26 +0100

    Replace the bind pose matrix inversions with old code that preserves the bone roll

--- BlenderIO/Import/ArmatureImport.py
+++ BlenderIO/Import/ArmatureImport.py
@@ -23,13 +23,30 @@ def import_skeleton(parent_obj, armature_name, model_data):
         if child_name in list_of_bones:
             continue
 
-        bone_matrix = np.linalg.inv(bone_matrices[child])
+        # This is just a complicated way of inverting the bone matrix...
+        # Using bone_matrix = np.linalg.inv(bone_matrices[child]) results in an incorrect bone roll.
+        # Should investigate this to do this properly, but... well, this works...
+        bm = bone_matrices[child]
+        pos = bm[:3, 3]
+        pos *= -1
+        rotation = bm[:3, :3]
+        pos = np.dot(rotation.T, pos)
+        bone_matrix = np.zeros((4, 4))
+        bone_matrix[3, :3] = pos
+        bone_matrix[:3, :3] = rotation.T
+        bone_matrix[3, 3] = 1
+
+        child_pos = pos
+
         bone = model_armature.data.edit_bones.new(child_name)
 
         list_of_bones[child_name] = bone
         bone.head = np.array([0., 0., 0.])
         bone.tail = np.array([0., 0.2, 0.])  # Make this scale with the model size in the future, for convenience
         bone.transform(Matrix(bone_matrix.tolist()))
+
+        bone.head = np.array([0., 0., 0.]) + child_pos
+        bone.tail = np.array(bone.tail) + child_pos
 
         if parent != -1:
             bone.parent = list_of_bones[model_data.skeleton.bone_names[parent]]
