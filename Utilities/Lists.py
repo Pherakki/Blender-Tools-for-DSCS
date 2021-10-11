6151808b9a1f99893119c21efb213dc608112707
Pherakki <71677509+Pherakki@users.noreply.github.com>
2021-10-12 00:09:46 +0100

    Add natural sort algorithm

--- Utilities/Lists.py
+++ Utilities/Lists.py
@@ -1,3 +1,6 @@
+import re
+
+
 def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
@@ -5,3 +8,12 @@ def chunks(lst, n):
 
 def flip_dict(dct):
     return {v: k for k, v in dct.items()}
+
+
+def natural_sort(l, accessor=lambda x: x):
+    """
+    Adapted from https://stackoverflow.com/a/4836734
+    """
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=lambda x: alphanum_key(accessor(x)))
