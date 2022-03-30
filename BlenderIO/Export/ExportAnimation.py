ef45327187650cbe9ac923972e8ffbf029ea03d5
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-03-30 19:08:49 +0100

    Remove capstone keyframe insertion

--- BlenderIO/Export/ExportAnimation.py
+++ BlenderIO/Export/ExportAnimation.py
@@ -183,11 +183,12 @@ def integerise_frame_indices(animation_channel, frame_default, interpolation_fun
     if required_frame_indices[0] != 0:
         required_frame_indices.insert(0, 0)
 
-    largest_idx = int(np.ceil(list(animation_channel_cpy.keys())[-1]))
-    if largest_idx < 0:
-        largest_idx = 0
-    if required_frame_indices[-1] != largest_idx:
-       required_frame_indices.append(largest_idx)
+    # # Add a capstone keyframe
+    # largest_idx = int(np.ceil(list(animation_channel_cpy.keys())[-1]))
+    # if largest_idx < 0:
+    #     largest_idx = 0
+    # if required_frame_indices[-1] != largest_idx:
+    #    required_frame_indices.append(largest_idx)
     interpolation_function = produce_interpolation_method_dict(animation_channel_cpy, frame_default, interpolation_function, debug_output)
 
     return {frame: interpolation_function(frame) for frame in required_frame_indices}
