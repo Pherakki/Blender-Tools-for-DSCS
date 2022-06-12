20ef123652c59f74585fb276af7a40483c1bac3b
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-06-12 16:38:12 +0100

    Add functions that might be able to properly integerise the animation frames automatically

--- BlenderIO/Export/ExportAnimation.py
+++ BlenderIO/Export/ExportAnimation.py
@@ -113,6 +113,85 @@ def fetch_subdata(data, bone_name, strip_single_frame_transforms, required_subtr
     return subdata
 
 
+def get_max_keyframe(animation_data, animation_uv_data):
+    max_frame = 0.
+    # First do animation data
+    for bone_name, group in animation_data.items():
+        for curve_type, curve_data in group.items():
+            frame_indices = np.array(list(curve_data.keys()))
+            if len(frame_indices) < 2:
+                continue
+            max_frame = max(max(frame_indices), max_frame)
+
+    # Now do shader channel data
+    for shader_channel_idx, curve_data in animation_uv_data.items():
+        frame_indices = np.array(list(curve_data.keys()))
+        if len(frame_indices) < 2:
+            continue
+        max_frame = max(max(frame_indices), max_frame)
+    return max_frame
+
+
+def get_all_diffable_keyframes(animation_data, animation_uv_data):
+    keyframes = []
+    # First do animation data
+    for bone_name, group in animation_data.items():
+        for curve_type, curve_data in group.items():
+            frame_indices = np.array(list(curve_data.keys()))
+            if len(frame_indices) < 2:
+                continue
+            keyframes.append(frame_indices)
+
+    # Now do shader channel data
+    for shader_channel_idx, curve_data in animation_uv_data.items():
+        frame_indices = np.array(list(curve_data.keys()))
+        if len(frame_indices) < 2:
+            continue
+        keyframes.append(frame_indices)
+    return keyframes
+
+
+def get_frame_indices_stretch_factor(all_keyframes, tol, max_n):
+    # This isn't perfect: Step 1 will blow up to max_n if tol is too strict
+    # To fix this, need to figure out the correct tol / dynamic tol
+
+    flat_keyframes = np.array([subitem for item in all_keyframes for subitem in item])
+    if not len(flat_keyframes):
+        return 1.
+
+    # Step 1: Get all keyframes within a certain frame bucket
+    total_product = 1.
+    converged = False
+    differences = [(keyframes[1:] - keyframes[:-1]) for keyframes in all_keyframes]
+    differences = np.array([subitem for item in differences for subitem in item])
+    while not converged:
+        converged = True
+        fdifferences = differences * total_product
+        errors = np.abs(fdifferences - np.around(fdifferences))
+        for smallest_difference in sorted(errors):
+            if smallest_difference > tol:
+                converged = False
+                break
+
+        if not converged:
+            candidate_product = total_product * (1 / smallest_difference)
+            if candidate_product > max_n:
+                converged = True
+                total_product = max_n
+            else:
+                total_product = candidate_product
+
+
+    # Step 2: Optimise the value of N
+    # Equation is i = xN
+    # Can solve for N by calculating N = (i . x) / (x . x)
+    new_keyframes = np.around(flat_keyframes * total_product)
+    fac_1 = np.dot(flat_keyframes, new_keyframes)
+    fac_2 = np.dot(flat_keyframes, flat_keyframes)
+
+    end_result = fac_1 / fac_2
+    return end_result
+
 def get_smallest_frame_delta(animation_data, animation_uv_data):
     smallest_frame_delta = np.inf
 
