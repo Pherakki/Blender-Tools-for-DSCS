ab9e8895f4fa7fc7182acbd5e594226c3a37a29e
Pherakki <71677509+Pherakki@users.noreply.github.com>
2022-11-08 22:35:09 +0000

    Add a shortcut for formatting potentially-null values

--- src/Core/serialization/utils.py
+++ src/Core/serialization/utils.py
@@ -4,3 +4,7 @@ def chunk_list(lst, chunksize):
 
 def flatten_list(lst):
     return [subitem for item in lst for subitem in item]
+
+
+def safe_format(obj, formatter):
+    return f"{obj if obj is None else formatter(obj)}"
