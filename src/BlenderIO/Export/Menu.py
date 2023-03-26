3753c25b02f1db94f5c0e39c716344960131206a
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-03-26 23:33:36 +0100

    ADD: Start of export refactor

---
+++ src/BlenderIO/Export/Menu.py
@@ -0,0 +1,16 @@
+import bpy
+
+
+class MVExportSubmenu(bpy.types.Menu):
+    bl_idname = "OBJECT_MT_MediaVision_export_submenu"
+    bl_label = "Media.Vision"
+
+    def draw(self, context):
+        layout = self.layout
+        #layout.operator(ExportDSCS.bl_idname, text="DSCS Model (.name)")
+        # layout.operator(ExportMegido.bl_idname, text="Megido 72 Model (.name)")
+
+
+
+def menu_func_export(self, context):
+    self.layout.menu(MVExportSubmenu.bl_idname)
