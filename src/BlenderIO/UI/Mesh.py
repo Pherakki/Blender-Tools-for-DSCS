b66722f55ab05ffa71aa3409ca894ed7c3336f7f
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-04-28 21:10:51 +0100

    ADD: Mesh UI

---
+++ src/BlenderIO/UI/Mesh.py
@@ -0,0 +1,29 @@
+import bpy
+
+
+class OBJECT_PT_DSCSMeshPanel(bpy.types.Panel):
+    bl_label       = "DSCS Mesh"
+    bl_idname      = "OBJECT_PT_DSCSMeshPanel"
+    bl_space_type  = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context     = "data"
+    bl_options     = {'DEFAULT_CLOSED'}
+    
+    @classmethod
+    def poll(self, context):
+        return context.mesh is not None
+
+    def draw(self, context):
+        mat = context.mesh
+        layout = self.layout
+        props = mat.DSCS_MeshProperties
+        
+        layout.prop(props, "name_hash")
+        
+        layout.prop(props, "is_rendered")
+        layout.prop(props, "is_wireframe")
+        layout.prop(props, "flag_3")
+        layout.prop(props, "flag_4")
+        layout.prop(props, "flag_5")
+        layout.prop(props, "flag_6")
+        layout.prop(props, "flag_7")
