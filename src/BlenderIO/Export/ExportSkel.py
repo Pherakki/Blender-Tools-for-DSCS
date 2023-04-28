f4f583b9eca854966c0d038915ffbbebb0d05364
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-04-28 21:09:18 +0100

    ADD: Skeleton export code

---
+++ src/BlenderIO/Export/ExportSkel.py
@@ -0,0 +1,36 @@
+import bpy
+from mathutils import Matrix
+
+from ...Core.FileFormats.Skel.SkelInterface import SkelInterface
+from ...Utilities.Hash import dscs_hash_string
+
+
+def create_bpy_to_dscs_bone_map(armature_obj):
+    return {bone.name: bidx for bidx, bone in enumerate(armature_obj.data.bones)}
+
+
+def extract_skel(armature_obj, base_anim, errorlog, bpy_to_dscs_bone_map):
+    armature = armature_obj.data
+    props = armature.DSCS_ModelProperties
+    si = SkelInterface()
+    
+    bone_indices = {bone.name: i for i, bone in enumerate(armature.bones)}
+    for bidx, bone in enumerate(armature.bones):
+        parent = bone.parent
+        parent_idx = -1 if parent is None else bone_indices.get(parent.name, -1)
+        
+        pos   = list(base_anim.locations[bidx].values())[0]
+        quat  = list(base_anim.rotations[bidx].values())[0]
+        scale = list(base_anim.scales[bidx].values())[0]
+        
+        si.add_bone(dscs_hash_string(bone.name), 
+                    parent_idx, 
+                    bone.DSCS_BoneProperties.flag,
+                    [*pos, 1.], 
+                    quat, 
+                    [*scale, 1.])
+    
+    for fc in props.float_channels:
+        si.add_float_channel(fc.name_hash, fc.flags, (fc.channel << 8) | fc.array_index)
+
+    return si
