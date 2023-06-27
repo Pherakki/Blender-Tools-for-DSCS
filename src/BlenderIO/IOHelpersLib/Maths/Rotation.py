30abd20acf0d03ed78476661551af72b92e81e1b
Pherakki <71677509+Pherakki@users.noreply.github.com>
2023-06-27 23:34:39 +0100

    ADD: Euler/quat conversion functions

--- src/BlenderIO/IOHelpersLib/Maths/Rotation.py
+++ src/BlenderIO/IOHelpersLib/Maths/Rotation.py
@@ -1,6 +1,14 @@
 from mathutils import Quaternion, Euler
 
 
+def euler_to_quat(rotation_euler, rotation_mode):
+    return Euler(rotation_euler, rotation_mode).to_quaternion()
+
+
+def quat_to_euler(rotation_quat, rotation_mode):
+    return Quaternion(rotation_quat).to_euler(rotation_mode)
+
+
 def convert_rotation_to_quaternion(rotation_quat, rotation_euler, rotation_mode):
     if rotation_mode == "QUATERNION":
         # pull out quaternion data, normalise
@@ -8,4 +16,4 @@ def convert_rotation_to_quaternion(rotation_quat, rotation_euler, rotation_mode)
         mag = sum(e**2 for e in q)
         return Quaternion([e/mag for e in q])
     else:
-        return Euler(rotation_euler, rotation_mode).to_quaternion()
+        return euler_to_quat(rotation_euler, rotation_mode)
--- src/BlenderIO/IOHelpersLib/Maths/__init__.py
+++ src/BlenderIO/IOHelpersLib/Maths/__init__.py
@@ -1,4 +1,4 @@
-from .Rotation import convert_rotation_to_quaternion
-from .TransformMatrix import decomposableToTRS
 from .Interpolation import lerp, slerp, interpolate_keyframe_dict
 from .RationalApproximation import rational_approx_brute_force
+from .Rotation import convert_rotation_to_quaternion, quat_to_euler, euler_to_quat
+from .TransformMatrix import decomposableToTRS
